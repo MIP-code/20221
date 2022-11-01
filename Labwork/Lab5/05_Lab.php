818874aa4a38a8604660406e85b613edb261aa2e
Mo <81822684+MIP-code@users.noreply.github.com>
2022-11-01 13:52:51 +0300

    new fix

--- Labwork/Lab5/05_Lab.php
+++ Labwork/Lab5/05_Lab.php
@@ -1,25 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab_05</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
 <?php
 include("Course.php");
 
-$courseOne= new Course();
-$courseOne->setCourseCode('CIS 201');
-$courseOne->setCourseName('Web Design');
-$courseOne->setCourseDescription('Building websites');
-$courseOne->setGrade('A+');
+$courseOne= new Course("CIS 201","Web Design","Building websites","A+");
 
-$courseTwo= new Course();
-$courseTwo->setCourseCode('CIS 221');
-$courseTwo->setCourseName('Database systems');
-$courseTwo->setCourseDescription('Building databeases');
-$courseTwo->setGrade('A');
+$courseTwo= new Course("CIS 221","Database","Building databases","A");
 
-$courseThree= new Course();
-$courseThree->setCourseCode('CIS 202');
-$courseThree->setCourseName('Data Structures');
-$courseThree->setCourseDescription('Learning about he concepts and algorithms of data');
-$courseThree->setGrade('A');
+$courseThree= new Course("CIS 202", "Data Structures","Learning about he concepts and algorithms of data","A");
 
-
+$myArray=array($courseOne,$courseTwo,$courseThree);
 
 
 echo"<table border = '1' width='500'>";
@@ -30,48 +31,13 @@ echo "<tr>";
             echo "<th>Course Description</th>";
             echo "<th>Grade</th>";
 echo "</tr>";
-
-        echo "<tr>";
-            echo '<td>'.$courseOne->getCourseCode();'</td>';
-            echo '<td>'.$courseOne->getCourseName();'</td>';
-            echo '<td>'.$courseOne->getCourseDescription();'</td>';
-            echo '<td>'.$courseOne->getGrade();'</td>';
-        echo "</tr>";
-
-        echo "<tr>";
-            echo '<td>'.$courseTwo->getCourseCode();'</td>';
-            echo '<td>'.$courseTwo->getCourseName();'</td>';
-            echo '<td>'.$courseTwo->getCourseDescription();'</td>';
-            echo '<td>'.$courseTwo->getGrade();'</td>';
-        echo "</tr>";
-
-        echo "<tr>";
-            echo '<td>'.$courseThree->getCourseCode();'</td>';
-            echo '<td>'.$courseThree->getCourseName();'</td>';
-            echo '<td>'.$courseThree->getCourseDescription();'</td>';
-            echo '<td>'.$courseThree->getGrade();'</td>';
-        echo "</tr>";
-
+    foreach($myArray as $myArray){
+        echo $myArray->cTable();
+    }
+      
 echo"</table>";
 
 ?>
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab_05</title>
-</head>
-<body>
-
-
 
 
 
--- Labwork/Lab5/Course.php
+++ Labwork/Lab5/Course.php
@@ -6,33 +6,38 @@ protected $courseName;
 protected $courseDescription;
 protected $grade;
 
-
-//methods for the attributes
-
-function getCourseCode(){
-    return $this->courseCode;
+function __constructer($courseCode,$courseName,$courseDescription,$grade ){
+    $this->courseCode=$courseCode;
+    $this->courseName=$courseName;
+    $this->courseDescription=$courseDescription;
+    $this->grade=$grade;
 }
 
+//methods for the attributes
 function setCourseCode($courseCode){
     $this->courseCode =$courseCode;
 }
 
-function getCourseName(){
-    return $this->courseName;
+function getCourseCode(){
+    return $this->courseCode;
 }
 
 function setCourseName($courseName){
     $this->courseName =$courseName;
 }
 
-function getCourseDescription(){
-    return $this->courseDescription;
+function getCourseName(){
+    return $this->courseName;
 }
+
 
 function setCourseDescription($courseDescription){
     $this->courseDescription =$courseDescription;
 }
 
+function getCourseDescription(){
+    return $this->courseDescription;
+}
 
 
 function getGrade(){
@@ -43,9 +48,30 @@ function setGrade($grade){
     $this->grade =$grade;
 }
 
-function highestGrade($grade){
-    if ($grade == "A+") {
+function hGrade(){
+    if ($this->getGrade() == 'A+') {
         return true;
+    }
+}
+
+function cTable(){
+    if ($this->hGrade() == true) {
+        echo "<tr class=Y>";
+            echo "<td>".$this->getCourseCode()."</td>";
+            echo "<td>".$this->getCourseName()."</td>";
+            echo "<td>".$this->getCourseDescription()."</td>";
+            echo "<td>".$this->getGrade()."</td>";
+        echo "</tr>";
+    
+ 
+    }else{
+        echo "<tr>";
+            echo "<td>".$this->getCourseCode()."</td>";
+            echo "<td>".$this->getCourseName()."</td>";
+            echo "<td>".$this->getCourseDescription()."</td>";
+            echo "<td>".$this->getGrade()."</td>";
+        echo "</tr>";
+
     }
 }
 
