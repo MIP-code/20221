7cf6af6fa0b389a220033e075c9d219393eaf0d4
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2022-12-21 21:00:24 +0300

    updating bookingpage

--- Project/src/php/booking_action.php
+++ Project/src/php/booking_action.php
@@ -1,5 +1,15 @@
 <?php
-include('db_login');
+include('./db_login.php');
 
 
-?>
+if (isset($_POST['name'])){
+    $trainer = $_POST['name'];
+    $plan = $_POST['plan'];
+
+    $query = "INSERT INTO `g` (`trainer_name`, `plan_duration`) VALUES ('$trainer', '$plan')";
+    $test = "SELECT `number_of_trainees` FROM `g` WHERE `trainer_name` = $trainer AND `plan` = $plan";
+
+    mysqli_query($connection, $query);
+    header("Refresh: 3; url= ../../www/homepage.php", true, 301);
+    echo "You have successfully registered with $trainer for $plan months";
+}
--- Project/www/bookingPage.php
+++ Project/www/bookingPage.php
@@ -1,75 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Booking Page</title>
-    <?php
-    include('index.php');
-    ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Booking Page</title>
+  <?php
+  include('index.php');
+  ?>
 
-<link rel="stylesheet" href="../src/css/bookingPage.css">
+  <link rel="stylesheet" href="../src/css/bookingPage.css">
 </head>
+
 <body>
+  <div style="width:800px; margin:0 auto;">
+    <div class="select">
 
-<div class="select">
-    
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <label class="input-group-text" for="inputGroupSelect01">Trainer</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01">Choose your trainer and the plan</label>
+          </br>
+        </div>
+        <form method="POST" action="../src/php/booking_action.php">
+          <select class="custom-select" id="inputGroupSelect01" name="name">
+            <option selected>Choose a trainer</option>
+            <option value="Jeff">Jeff</option>
+            <option value="Matt">Matt</option>
+            <option value="David">David</option>
+          </select>
+          <select class="custom-select" id="inputGroupSelect01" name="plan">
+            <option selected>Choose a plan</option>
+            <option value="1">1 Month</option>
+            <option value="3">3 Months</option>
+            <option value="6">6 Months</option>
+            <option value="12">12 Months</option>
+          </select>
+          <div class="select">
+            <div class="input-group mb-3">
+              <div class="input-group-append">
+                <input class="btn btn-warning btn-lg btn-block" type="submit" value="Submit">
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
-  <form method="POST" action="">
-  <select class="custom-select" id="inputGroupSelect01">
-    <option selected>Choose a trainer</option>
-    <option value="1">Jeff</option>
-    <option value="2">Matt</option>
-    <option value="3">David</option>
-  </select>
-</form>
-
-</div>
-</div>
-<div class="select">
-    
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <label class="input-group-text" for="inputGroupSelect01">Day</label>
-  </div>
-  <form method="POST" action="">
-  <select class="custom-select" id="inputGroupSelect01">
-    <option selected>Choose a day</option>
-    <option value="1">Sunday</option>
-    <option value="2">Monday</option>
-    <option value="3">Tuesday</option>
-    <option value="4">Wednsday</option>
-    <option value="5">Thursday</option>
-    <option value="6">Friday</option>
-    <option value="7">Saturday</option>
-  </select>
-</form>
-</div>
-</div>
-
-<div class="select">
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <label class="input-group-text" for="inputGroupSelect01">Time</label>
-  </div>
-  <form method="POST" action="">
-  <select class="custom-select" id="inputGroupSelect01">
-    <option selected>Choose a time</option>
-    <option value="1">1:00pm - 3:00pm </option>
-    <option value="2">4:00pm - 7:00pm</option>
-    <option value="3">8:00pm - 11:00pm</option>
-  </select>
-  </form>
-</div>
-<div class="input-group-append">
-  <input class="btn btn-warning btn-lg btn-block" type="submit" value="Submit">
-  </div>
-</div>
-
 </body>
-</html>
 
+</html>
